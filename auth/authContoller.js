1098eb0c371ce797cbd39432bf14a8781965b2d3
Your Name <you@example.com>
2026-03-23 17:49:38 +0530

    new udpated code

--- auth/authContoller.js
+++ auth/authContoller.js
@@ -138,7 +138,7 @@ router.post("/login", async (req, res) => {
       httpOnly: true,
       secure: true,
       sameSite: "none",
-      // domain: ".reviewbadhao.com",
+      domain: ".reviewbadhao.com",
       path: "/",
       maxAge: 100 * 365 * 24 * 60 * 60 * 1000,
     });
